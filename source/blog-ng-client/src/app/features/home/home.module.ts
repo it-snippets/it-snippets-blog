219b627b6cb09577916a95cee540ee2ea0573996
Piotr Sztypa <piotr@sztypa.pl>
2021-03-12 23:02:52 +0100

    Fix routing

--- source/blog-ng-client/src/app/features/home/home.module.ts
+++ source/blog-ng-client/src/app/features/home/home.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './container/home.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    RouterModule.forChild([
+      { path: '', component: HomeComponent}
+    ])
   ],
   declarations: [HomeComponent]
 })
