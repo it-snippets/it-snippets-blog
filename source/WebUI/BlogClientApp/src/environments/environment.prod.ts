e7d1a17359f77abed5983a9df6b2af7fe9221c42
Piotr Sztypa <piotr@sztypa.pl>
2021-03-19 22:26:30 +0100

    Fix typo

--- source/WebUI/BlogClientApp/src/environments/environment.prod.ts
+++ source/WebUI/BlogClientApp/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiUrl: '#{apiUrl}#',
   appInsights: {
-    instrumentationKey: '#{apappInsights.instrumentationKey}#',
+    instrumentationKey: '#{appInsights.instrumentationKey}#',
   },
 };
